fdfcd5b833792c3c7910eec74b5cd2f5eb03ed63
18964104925 <1105735390@qq.com>
2023-03-23 20:29:07 +0800

    2023/3/23

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,6 +9,7 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    Block: typeof import('./components/block.vue')['default']
     README: typeof import('./components/README.md')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
