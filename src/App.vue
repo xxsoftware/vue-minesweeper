1c2060e570d97a9ddb4b75b87bcbe3fd0d35dcb2
18964104925 <1105735390@qq.com>
2023-03-26 16:46:29 +0800

    2023/3/26

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,9 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
+const title = computed(() => isDark.value ? 'Good Evening' : 'Good Morning')
 useHead({
-  title: 'Vitesse',
+  title,
   meta: [
     { name: 'description', content: 'Opinionated Vite Starter Template' },
     {
