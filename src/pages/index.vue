34dcde6b7ad05e29aeb708e3e370164e507855ce
18964104925 <1105735390@qq.com>
2023-03-27 23:58:11 +0800

    2023/3/27

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,16 +4,13 @@ import { MinePlay } from '~/composables/MineLogic'
 const play = new MinePlay(10, 10)
 useStorage('minesweeper-state', play.state)
 const state = computed(() => play.board)
-// const music = new Audio()
-// music.src = 'https://m801.music.126.net/20230326003835/0db94b54aca73c252e3b5a7ea075b733/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/25197371564/e0a3/7b75/72e5/d0e93789410c177c4bb1455644fcb43c.m4a'
-// music.loop = true
-// music.volume = 0.05
 </script>
 
 <template>
   <div>
-    <div>Minesweeper</div>
-    <!-- <audio  controls loop="true" src="https://m801.music.126.net/20230326003835/0db94b54aca73c252e3b5a7ea075b733/jdyyaac/obj/w5rDlsOJwrLDjj7CmsOj/25197371564/e0a3/7b75/72e5/d0e93789410c177c4bb1455644fcb43c.m4a" /> -->
+    <div>
+      Minesweeper
+    </div>
     <div flex="~ gap-1" justify-center p5>
       <div @click="toggleDev()">
         <div v-show="isDev" i-mdi-eye w-8 h-8 />
